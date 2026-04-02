5d928846ee43b481fda70a3a534de67ef6b4ade8
nnajiehah07 <nnajiehah07@gmail.com>
2026-04-02 18:48:26 +0800

    Define Location class with properties
    Fix class declaration and add fields for location.

--- Location.java
+++ Location.java
@@ -1,1 +1,12 @@
+package model;
+
+public classs Location{
+  public string id;
+  public string name;
+  public double x;
+  public double y;
+}
+
+//Constructor
+//Straight-line distance to another location 
 
